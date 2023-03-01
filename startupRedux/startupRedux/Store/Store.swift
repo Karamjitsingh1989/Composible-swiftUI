8f62d88ec478aee2d70ec54536c3ff40fa6d46b9
Karamjit Singh <karamjitsingh1989@gmail.com>
2023-03-01 22:19:23 +0530

    changes

---
+++ startupRedux/startupRedux/Store/Store.swift
@@ -0,0 +1,28 @@
+//
+//  Store.swift
+//  startupRedux
+//
+//  Created by Karamjit Singh on 01/03/23.
+//
+
+import Foundation
+
+struct State {
+    var counter: Int = 0
+    
+}
+
+class Store {
+    
+    
+    
+    
+    
+}
+
+
+protocol Action{}
+func reducer(state: State, action: Action) -> State {
+    
+    return state
+}
---
+++ startupRedux/startupRedux/startupReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  startupReduxApp.swift
+//  startupRedux
+//
+//  Created by Karamjit Singh on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct startupReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
